7d0111a1deb0d62dcca233c1e3c42f2d59f28628
PranjaliGhode8 <pranjalighodefeb8@gmail.com>
2026-07-14 13:40:29 +0530

    Replace navbar Sign In avatar logo with clean text button project-wide

--- firebase-config.js
+++ firebase-config.js
@@ -74,11 +74,7 @@ onAuthStateChanged(auth, (user) => {
     }
   } else {
     // User is signed out
-    userBtn.innerHTML = `
-      <svg viewBox="0 0 24 24" style="width:24px;height:24px;fill:none;stroke:var(--text);stroke-width:2;">
-        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"/>
-        <circle cx="12" cy="7" r="4"/>
-      </svg>`;
+    userBtn.innerHTML = `<span class="user-btn-label">Sign In</span>`;
     userBtn.onclick = () => { window.location.href = 'auth.html'; };
     if (userDropdown) {
       userDropdown.classList.remove('show');
