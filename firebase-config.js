e4ebc781a9d009300b8920f014b1798d97f690e7
PranjaliGhode8 <pranjalighodefeb8@gmail.com>
2026-07-15 00:43:44 +0530

    Update welcome greeting parser to display first name only

--- firebase-config.js
+++ firebase-config.js
@@ -94,8 +94,9 @@ onAuthStateChanged(auth, (user) => {
     else if (hour >= 17 && hour < 21) greeting = "Good evening";
     else if (hour >= 21 || hour < 5) greeting = "Good night";
 
-    const name = user ? (user.displayName || user.email.split('@')[0]) : "friend";
-    welcomeText.textContent = `${greeting}, ${name}!`;
+    const rawName = user ? (user.displayName || user.email.split('@')[0]) : "friend";
+    const firstName = rawName.trim().split(/\s+/)[0];
+    welcomeText.textContent = `${greeting}, ${firstName}! 🍃`;
     welcomeBanner.style.display = 'flex';
   }
 });
